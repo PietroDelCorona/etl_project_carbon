f7667daa2e6449c132c4d6d62eecd8f6abc7c59a
Pietro Del Corona <pietrogdelcorona@gmail.com>
2024-12-01 00:32:38 -0300

    minio properties and VARCHAR fields

--- init_trino/create_schemas.sql
+++ init_trino/create_schemas.sql
@@ -1,11 +1,12 @@
 create schema if not exists minio.prod_bronze with (location = 's3a://bronze/');
 create schema if not exists minio.prod_silver with (location = 's3a://silver/');
 
+
 create table if not exists minio.prod_bronze.co2_emission_per_capita (
     entity VARCHAR,
     code VARCHAR,
-    year INT,
-    emissions_total_per_capita DECIMAL(10,8)
+    year VARCHAR,
+    emissions_total_per_capita VARCHAR
 ) WITH (
             external_location = 's3a://bronze/co2_emissions/',
             format = 'CSV',
@@ -16,9 +17,9 @@ create table if not exists minio.prod_bronze.co2_emission_per_capita (
 create table if not exists minio.prod_bronze.co2_land_use (
     entity VARCHAR,
     code VARCHAR,
-    year INT,
-    emissions_from_land_use_change DECIMAL(10,2),
-    emissions_total DECIMAL(10,2)
+    year VARCHAR,
+    emissions_from_land_use_change VARCHAR,
+    emissions_total VARCHAR
 ) WITH (
             external_location = 's3a://bronze/co2_land_use/',
             format = 'CSV',
@@ -29,10 +30,10 @@ create table if not exists minio.prod_bronze.co2_land_use (
 create table if not exists minio.prod_bronze.energy_mix_sources (
     entity VARCHAR,
     code VARCHAR,
-    year INT,
-    fossil_fuels_per_capita__kwh DECIMAL(10,5),
-    nuclear_per_capita__kwh__equivalent DECIMAL(10,5),
-    renewables_per_capita__kwh__equivalent DECIMAL(10,5)
+    year VARCHAR,
+    fossil_fuels_per_capita__kwh VARCHAR,
+    nuclear_per_capita__kwh__equivalent VARCHAR,
+    renewables_per_capita__kwh__equivalent VARCHAR
 ) WITH (
             external_location = 's3a://bronze/energy_mix/',
             format = 'CSV',
@@ -42,14 +43,14 @@ create table if not exists minio.prod_bronze.energy_mix_sources (
 
 create table if not exists minio.prod_bronze.population (
     country_or_area VARCHAR,
-    year INT,
+    year VARCHAR,
     area VARCHAR,
     sex VARCHAR,
     record_type VARCHAR,
     reliability VARCHAR,
-    source_year INT,
-    value INT,
-    value_footnotes INT
+    source_year VARCHAR,
+    value VARCHAR,
+    value_footnotes VARCHAR
 ) WITH (
             external_location = 's3a://bronze/population/',
             format = 'CSV',
@@ -59,8 +60,8 @@ create table if not exists minio.prod_bronze.population (
 
 create table if not exists minio.prod_silver.cleansed_co2_emission_per_capita (
     entity VARCHAR,
-    year INT,
-    emissions_total_per_capita DECIMAL(10,8)
+    year VARCHAR,
+    emissions_total_per_capita VARCHAR
 ) WITH (
             external_location = 's3a://silver/co2_emissions/',
             format = 'CSV',
@@ -70,9 +71,9 @@ create table if not exists minio.prod_silver.cleansed_co2_emission_per_capita (
 
 create table if not exists minio.prod_silver.cleansed_co2_land_use (
     entity VARCHAR,
-    year INT,
-    emissions_from_land_use_change DECIMAL(10,2),
-    emissions_total DECIMAL(10,2)
+    year VARCHAR,
+    emissions_from_land_use_change VARCHAR,
+    emissions_total VARCHAR
 ) WITH (
             external_location = 's3a://silver/co2_land_use/',
             format = 'CSV',
@@ -82,10 +83,10 @@ create table if not exists minio.prod_silver.cleansed_co2_land_use (
 
 create table if not exists minio.prod_silver.cleansed_energy_mix_sources (
     entity VARCHAR,
-    year INT,
-    fossil_fuels_per_capita__kwh DECIMAL(10,5),
-    nuclear_per_capita__kwh__equivalent DECIMAL(10,5),
-    renewables_per_capita__kwh__equivalent DECIMAL(10,5)
+    year VARCHAR,
+    fossil_fuels_per_capita__kwh VARCHAR,
+    nuclear_per_capita__kwh__equivalent VARCHAR,
+    renewables_per_capita__kwh__equivalent VARCHAR
 ) WITH (
             external_location = 's3a://silver/energy_mix/',
             format = 'CSV',
@@ -95,9 +96,9 @@ create table if not exists minio.prod_silver.cleansed_energy_mix_sources (
 
 create table if not exists minio.prod_silver.cleansed_world_population (
     coutry_or_area VARCHAR,
-    year INT,
+    year VARCHAR,
     area VARCHAR,
-    value INT
+    value VARCHAR
 ) WITH (
             external_location = 's3a://silver/population/',
             format = 'CSV',
